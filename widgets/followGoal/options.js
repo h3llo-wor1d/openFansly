b8783be4ccca4fa0032c031aa6821caf6cbfe267
willow-rubenstein <contact.notashe@gmail.com>
2022-10-10 20:25:03 -0400

    final commit

---
+++ widgets/followGoal/options.js
@@ -0,0 +1,9 @@
+// READ 'README.MD' BEFORE ATTEMPTING TO MESS WITH THESE OPTIONS!!!
+
+document.styleOptions = {
+    fontFile: "roobert.ttf",
+    defaultBarColor: "lime",
+    followerGoal: 100,
+    followerGoalMessage: "Fansly Follower Goal",
+    username: "h3llo_wor1d"
+}
